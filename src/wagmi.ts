e1349214d44a5d39f85351a7884122fbfd93ec03
jdhyun09 <jdhyun09@naver.com>
2025-02-27 18:01:52 +0900

    feat: add mammothon network

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,22 +1,37 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import {
-  arbitrum,
-  base,
   mainnet,
-  optimism,
-  polygon,
   sepolia,
+  Chain,
 } from 'wagmi/chains';
+
+export const mammothon = {
+  id: 55550,
+  name: 'Mammothon-g2-testnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Ethereum',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc-mammothon-g2-testnet-4a2w8v0xqy.t.conduit.xyz'],
+    }
+  },
+  blockExplorers: {
+    default: {
+      name: 'Explorer',
+      url: 'https://explorer-mammothon-g2-testnet-4a2w8v0xqy.t.conduit.xyz',
+    },
+  },
+} as const satisfies Chain;
 
 export const config = getDefaultConfig({
   appName: 'WEB3ITE',
   projectId: 'YOUR_PROJECT_ID',
   chains: [
     mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
+    mammothon,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
   ],
   ssr: true,
