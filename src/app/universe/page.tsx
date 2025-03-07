d07672e09bc1eb366042c3f6a3b7db5f75d7e600
ssaid <swywssaid@gmail.com>
2025-03-07 14:00:12 +0900

    refactor: Apply interactive design in universe page

--- src/app/universe/page.tsx
+++ src/app/universe/page.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import dynamic from 'next/dynamic';
 import TypingText from '@/components/TypingText';
+import { useMediaQuery } from 'usehooks-ts';
 
 // Header + GalaxyScene (R3F) Components
 const GalaxyScene = dynamic(() => import('@/components/GalaxyScene'), {
@@ -12,7 +13,18 @@ const Header = dynamic(() => import('@/components/layout/Header'), {
   ssr: false,
 });
 
-const longText = `This galaxy is called CelestaraX.
+const mobileText = `This galaxy is called CelestaraX.
+CelestaraX is the name of the Eternal On-Chain Page Hub project,
+a fusion of Celestia + Star (page) 
++ Era (a new era of fully on-chain pages).<br/>
+Here, you can experience the true Web3 era of pages.<br/>
+Below is a list of the top 4 most popular pages right now:<br/>
+<span style="color: #ff00ff;">Onchain Million Dollar</span><br/>  <!-- Hot Pink -->
+<span style="color: #00ffff;">Flappy Square</span><br/>  <!-- Neon Blue -->
+<span style="color: #ffcc00;">Space Shooter</span><br/>  <!-- Yellow -->
+<span style="color: #ff4444;">Hello World</span>`;
+
+const desktopText = `This galaxy is called CelestaraX.
 CelestaraX is the name of the Eternal On-Chain Page Hub project,
 a fusion of Celestia + Star (page) + Era (a new era of fully on-chain pages).<br/>
 Here, you can experience the true Web3 era of pages.<br/>
@@ -26,6 +38,7 @@ export default function UniversePage() {
   const router = useRouter();
   const [focusPlanet, setFocusPlanet] = useState(false);
   const [opacity, setOpacity] = useState(1);
+  const isMobile = useMediaQuery('(max-width: 640px)');
 
   const handleZoomComplete = () => {
     router.push('/explorer');
@@ -49,14 +62,28 @@ export default function UniversePage() {
     <div>
       <Header onDeployClick={handleDeployClick} />
       <main className='flex h-[calc(100vh-100px)]'>
-        {/* 📝 Typing Text Box */}
-        <div
-          className='font-size text-md absolute left-[500px] top-[200px] min-h-[100px] w-[800px] -translate-x-1/2 transition-opacity duration-1000'
-          style={{ opacity }}
-        >
-          <TypingText text={longText} speed={15} />
-        </div>
-        <div className='flex-1'>
+        {isMobile ? (
+          <div>
+            {/* 📝 Typing Text Box */}
+            <div
+              className='font-size absolute left-[430px] min-h-[100px] w-[800px] -translate-x-1/2 text-[8px] transition-opacity duration-1000'
+              style={{ opacity }}
+            >
+              <TypingText text={mobileText} speed={15} />
+            </div>
+          </div>
+        ) : (
+          <div>
+            {/* 📝 Typing Text Box */}
+            <div
+              className='font-size text-md absolute left-[500px] top-[200px] min-h-[100px] w-[800px] -translate-x-1/2 transition-opacity duration-1000'
+              style={{ opacity }}
+            >
+              <TypingText text={desktopText} speed={15} />
+            </div>
+          </div>
+        )}
+        <div className='flex flex-1 items-center justify-center'>
           {/* 🌌 3D Scene */}
           <GalaxyScene
             zooming={focusPlanet}
--- src/components/GalaxyScene.tsx
+++ src/components/GalaxyScene.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { SceneContent } from './SceneContent';
+import { useMediaQuery } from 'usehooks-ts';
 
 interface GalaxySceneProps {
   zooming: boolean;
@@ -13,11 +14,17 @@ export default function GalaxyScene({
   zooming,
   onZoomComplete,
 }: GalaxySceneProps) {
+  const isMobile = useMediaQuery('(max-width: 640px)');
+
   return (
     <Canvas
       className='-z-5 absolute inset-0'
       shadows
-      camera={{ position: [900, 400, 1500], near: 1, far: 10000 }}
+      camera={{
+        position: isMobile ? [2500, 1000, 1500] : [900, 400, 1500],
+        near: 1,
+        far: 10000,
+      }}
       style={{ width: '100%', height: '100%' }}
     >
       <ambientLight intensity={3} />
