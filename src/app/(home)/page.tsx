7ec51b92ca231b035f4d1075a71091744c42288d
ssaid <swywssaid@gmail.com>
2025-03-08 11:49:25 +0900

    refactor: Apply interactive design in home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -12,16 +12,16 @@ export default function HomePage() {
   return (
     <main className='relative flex h-screen flex-col items-center justify-center text-white'>
       <div className='text-center'>
-        <div className='font-tilt glitch-effect text-7xl font-bold text-pink-500'>
+        <div className='glitch-effect font-tilt text-4xl font-bold text-pink-500 sm:text-7xl'>
           CELESTARAX
         </div>
-        <p className='mt-4 max-w-md text-center font-mono text-xl text-gray-300'>
+        <p className='mt-4 max-w-md text-center font-mono text-lg text-gray-300 sm:text-xl'>
           The Eternal On-Chain Page Hub
         </p>
 
         <button
           onClick={handleClick}
-          className='mt-10 transform bg-pink-500 px-8 py-3 text-lg font-bold tracking-wider text-white shadow-[0_0_10px_#ff00ff] transition-transform hover:scale-105 hover:bg-pink-400'
+          className='mt-10 transform bg-pink-500 px-5 py-2 text-lg font-bold tracking-wider text-white shadow-[0_0_10px_#ff00ff] transition-transform hover:scale-105 hover:bg-pink-400 sm:px-8 sm:py-3'
         >
           ENTER
         </button>
